30737ccad23516fe6b6ce33c11baaf3de103d091
wedyaan <wedyan-14@hotmail.com>
2025-03-08 21:08:48 +0300

    two

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -1,5 +1,5 @@
 import { HeroSectionType } from '@/lib/types/sections';
-import { resumeFileName } from '@/lib/utils/config';
+// import { resumeFileName } from '@/lib/utils/config';
 
 export const heroSection: HeroSectionType = {
   subtitle: 'Hi, my name is',
@@ -9,9 +9,9 @@ export const heroSection: HeroSectionType = {
   description:
     "I'm a passionate Front-End web developer with hands-on experience in building web applications using React.js & Next.js with TypeScript,JavaScript, TailwindCSS, and Prisma, with UI/UX designing.",
   specialText: 'Currently available for freelance',
-  cta: {
-    title: 'see my resume',
-    url: `/${resumeFileName}`,
-    hideInDesktop: true,
-  },
+  // cta: {
+  //   title: 'see my resume',
+  //   // url: `/${resumeFileName}`,
+  //   hideInDesktop: true,
+  // },
 };
